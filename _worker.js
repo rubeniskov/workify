a51a957dcc490da2733f5ec7a7e48f6e7376409b
Rubén López Gómez <loslopez1@icloud.com>
2016-09-14 12:15:49 +0200

    remove: unused files

--- _worker.js
+++
@@ -1,7 +0,0 @@
-process.stdin.on('data', function(chunk){
-   console.log('This is a worker chunk:', chunk);
-});
-
-process.stdout.write(new Buffer('jajaja'), 'binary', function(){
-    console.log(arguments);
-});
